430b57bc5ff2e85a9b5044516da57c89b2594bde
Come Raczy <come.raczy@gmail.com>
2024-05-17 12:46:14 -0700

    closes #2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<div class="app">
+	<div class="header">
+
+    </div>
+	<div style="flex-basis: 100%;height: 0"></div>
+	<div class="leftside">
+	</div>
+	<div class="main">
+		
+			<slot />
+		
+	</div>
+	<div class="rightside"></div>
+	<div style="flex-basis: 100%;height: 0"></div>
+	<div class="footer"></div>
+</div>
+
+<style>
+	div.app {
+		display: flex;
+		flex-wrap: wrap;
+		width: 100vw;
+		max-width: 100%;
+		justify-content: space-between;
+		height: 100%;
+		margin: 0;
+        padding: 0;
+	}
+    div.header {
+        height: 5vh;
+        width: 100%;
+        border: 5px solid blue;
+    }
+    div.leftside {
+        min-height: 10vh;;
+        width:15%;
+        border:5px solid green;
+    }
+	div.main {
+		width: 70%;
+		justify-content: center;
+		align-items: center;
+		margin: 0;
+        border: 5px solid red;
+	}
+	div.rightside {
+		flex-grow: 1;
+		min-height: 10vh;
+        margin: 0;
+        padding: 0;
+        border: 5px solid yellow;
+	}
+    div.footer {
+        height: 5vh;
+        width: 100%;
+        border: 5px solid black;
+    }
+</style>
